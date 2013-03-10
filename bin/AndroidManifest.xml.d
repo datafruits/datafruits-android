3b0743c080678fe16b6a1b2830427b7858cce574
Tony Miller <mcfiredrill@gmail.com>
2013-03-09 16:42:43 -0800

    first commit

---
+++ bin/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+/home/tony/src/datafruits.fm-android/bin/AndroidManifest.xml : \
+/home/tony/src/datafruits.fm-android/AndroidManifest.xml \
---
+++ bin/MainActivity-debug-unaligned.apk.d
@@ -0,0 +1,4 @@
+/home/tony/src/datafruits.fm-android/bin/MainActivity-debug-unaligned.apk : \
+/home/tony/src/datafruits.fm-android/bin/MainActivity.ap_ \
+/home/tony/src/datafruits.fm-android/bin/classes.dex \
+/opt/android-sdk/tools/support/annotations.jar \
